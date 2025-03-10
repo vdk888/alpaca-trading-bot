5c089485148b146a153ba79bed450bda98097527
vdk888 <114986296+vdk888@users.noreply.github.com>
2025-03-10 13:59:27 +0100

    download as from

--- telegram_bot.py
+++ telegram_bot.py
@@ -49,7 +49,7 @@ class TradingBot:
                 client = Client()
                 
                 # Try to get parameters from Object Storage
-                json_content = client.download_from_text("best_params.json")
+                json_content = client.download_as_text("best_params.json")
                 best_params_data = json.loads(json_content)
                 
                 if symbol in best_params_data:
@@ -1185,7 +1185,7 @@ Price Changes:
                 
                 # Try to get parameters from Object Storage
                 try:
-                    json_content = client.download_from_text("best_params.json")
+                    json_content = client.download_as_text("best_params.json")
                     best_params_data = json.loads(json_content)
                     logger.info("Successfully loaded best parameters from Object Storage")
                 except Exception as e:
